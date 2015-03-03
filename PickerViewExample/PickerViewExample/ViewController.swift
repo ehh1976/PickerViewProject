6cb0a49881d80379782fdfc65d7245f1ef058607
ehh1976 <ehh.1976@gmail.com>
2015-03-03 15:56:56 +0200

    Implement Protocols UIPickViewDataSource and UIPickerViewDelegate

--- PickerViewExample/PickerViewExample/ViewController.swift
+++ PickerViewExample/PickerViewExample/ViewController.swift
@@ -7,12 +7,26 @@
 //
 
 import UIKit
+struct NamedColor{
+    var name: String
+    var color: UIColor
+    }
 
-class ViewController: UIViewController, UIPickerViewDelegate {
-
+class ViewController: UIViewController, UIPickerViewDataSource,UIPickerViewDelegate {
+    @IBOutlet var pickerData: UIPickerView!
+    
+    let colors = [
+        NamedColor (name: "red", color: UIColor.redColor()),
+        NamedColor (name: "blue", color: UIColor.blueColor()),
+        NamedColor (name: "gray", color: UIColor.grayColor()),
+        NamedColor (name: "green", color: UIColor.greenColor())
+    ]
+   
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        pickerData.delegate = self
+        pickerData.dataSource = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,9 +34,26 @@ class ViewController: UIViewController, UIPickerViewDelegate {
         // Dispose of any resources that can be recreated.
     }
 
-    func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-        
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String! {
+        return colors[row].name
     }
-
+    
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return colors.count
+    }
+    
+    func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        self.view.backgroundColor = colors[row].color
+    }
+    
+//    func pickerView(pickerView: UIPickerView, attributedTitleForRow row: Int, forComponent component: Int) -> NSAttributedString? {
+//        let titleData = colors[row].name
+//        var myTitle = NSAttributedString(string: titleData, attributes: [NSFontAttributeName:UIFont(name: "Georgia", size: 15.0)!,NSForegroundColorAttributeName:colors[row].color])
+//        return myTitle
+//    }
 }
 
